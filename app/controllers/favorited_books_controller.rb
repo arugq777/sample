4b570dac430a01207e250dd82b84c037342aa3da
zzz <zenhexy@fastmail.fm>
2012-02-17 18:41:18 -0600

    Favorites work! Buttons and all

--- app/controllers/favorited_books_controller.rb
+++ app/controllers/favorited_books_controller.rb
@@ -5,16 +5,17 @@ class FavoritedBooksController < ApplicationController
 		@book = Book.find( params[:favorited_book][:favorited_id] )
 		current_user.add_favorite!(@book)
 		respond_to do |format|
-			format.html { redirect_to @book }
+			format.html { redirect_to @user }
 			format.js
 		end
 	end
 	
 	def destroy
-		favorite = FavoritedBook.find(params[:id])
-		current_user.del_favorite!(favorite)
+		@favorite = FavoritedBook.find(params[:id])
+		@book= Book.find(@favorite.favorited_id)
+		current_user.del_favorite!(@favorite)
 		respond_to do |format|
-			format.html { redirect_to @book }
+			format.html { redirect_to @user }
 			format.js
 		end
 	end
